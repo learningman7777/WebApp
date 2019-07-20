d2b4c073314ea716acf040d771b6445f49631d96
learningman7777 <learningman7777@gmail.com>
2019-07-21 01:19:14 +0900

    feat: Create Models

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Student()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Student(Student model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+    }
+}
---
+++ WebApp/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class Student
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Range(15,70)]
+        public int Age { get; set; }
+        [Required, MinLength(5)]
+        public int Country { get; set; }
+
+    }
+}
